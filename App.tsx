a0772c4d59bd1c9a5602b52042b42a33f5775c6b
Amr Abulseoud <amrabulseoud@gmail.com>
2020-04-13 09:53:40 +0200

    [FEAT]: created Sign in screen ui.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import * as eva from "@eva-design/eva"
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { SignInScreen } from "./src/screens/SignIn";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name={"Sign In"} component={SignInScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name={"Sign In"} component={SignInScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,47 @@
-import { Layout } from "@ui-kitten/components";
-import React from "react";
-import { Text } from "react-native";
+import { Button, Icon, Input, Layout } from "@ui-kitten/components";
+import React, { useRef } from "react";
+import { Image, TouchableWithoutFeedback } from "react-native";
 
 export const SignInScreen = () => {
+  const passwordRef = useRef<any>(null)
+  const [secureTextEntry, setSecureTextEntry] = React.useState(true);
+
+  const toggleSecureEntry = () =>
+    setSecureTextEntry(secureTextEntry => !secureTextEntry);
+
+  const PasswordIcon = (props: any) => (
+    <TouchableWithoutFeedback onPress={toggleSecureEntry}>
+      <Icon {...props} name={secureTextEntry ? 'eye-off' : 'eye'} />
+    </TouchableWithoutFeedback>
+  );
+
   return (
-    <Layout>
-      <Text>Sign in</Text>
+    <Layout style={{ height: '100%' }}>
+      <Layout style={{ marginHorizontal: 20, marginTop: 20 }}>
+
+        <Image style={{ alignSelf: 'center', aspectRatio: 4, height: 80, marginTop: 24 }}
+               source={require('../../assets/minstagram-logo.png')} />
+
+        <Input
+          style={{ marginTop: 16 }}
+          placeholder='Email'
+          textContentType="emailAddress"
+          autoCompleteType="email"
+          onSubmitEditing={() => passwordRef?.current?.focus()}
+        />
+
+        <Input
+          ref={passwordRef}
+          style={{ marginVertical: 16 }}
+          placeholder='Password'
+          textContentType="password"
+          autoCompleteType="password"
+          accessoryRight={PasswordIcon}
+          secureTextEntry={secureTextEntry}
+        />
+
+        <Button style={{ backgroundColor: "#F95E62" }}>Login</Button>
+      </Layout>
     </Layout>
   )
 }
