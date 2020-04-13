dfbb49772bef64c7705d1af1524a5fd549a64038
Amr Abulseoud <amrabulseoud@gmail.com>
2020-04-13 16:43:35 +0200

    [REFACTOR]: refactored navigation and added Home Navigation

--- App.tsx
+++ App.tsx
@@ -5,10 +5,41 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { SignInScreen } from "./src/screens/SignIn";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { store } from "./src/store";
+import { HomeScreen } from "./src/screens/Home";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createStackNavigator();
+const AuthStack = createStackNavigator();
+
+const AuthStackNavigator = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen name={"Sign In"} component={SignInScreen} />
+  </AuthStack.Navigator>
+)
+const HomeTabs = createBottomTabNavigator();
+
+const HomeNavigator = () => (
+  <HomeTabs.Navigator>
+    <HomeTabs.Screen name={"Home"} component={HomeScreen} />
+  </HomeTabs.Navigator>
+)
+
+const RootStack = createStackNavigator();
+
+const RootStackNavigator: React.FC = () => {
+  // @ts-ignore
+  const loggedIn = useSelector(state => state.auth.loggedIn)
+  console.log("RootNav: ", loggedIn)
+  return (
+    <RootStack.Navigator>
+      {loggedIn ?
+        <RootStack.Screen name="Home" component={HomeNavigator} /> :
+        <RootStack.Screen name="Sign In" component={AuthStackNavigator} options={{ headerShown: false }} />
+      }
+    </RootStack.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -17,9 +48,7 @@ export default function App() {
 
       <ApplicationProvider {...eva} theme={eva.light}>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name={"Sign In"} component={SignInScreen} />
-          </Stack.Navigator>
+          <RootStackNavigator />
         </NavigationContainer>
       </ApplicationProvider>
     </Provider>
